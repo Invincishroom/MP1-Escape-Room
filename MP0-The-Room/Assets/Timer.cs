254499852ce1f9096322e15b777bd7d78089146a
Invincishroom <u3612767@connect.hku.hk>
2026-02-19 07:28:17 -0600

    Android apk changes

--- MP0-The-Room/Assets/Timer.cs
+++ MP0-The-Room/Assets/Timer.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class Timer : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public static float timeRemaining = 10f;
+    public static float timeRemaining = 600f;
     public GameObject TimerTextObject;
     public Transform LossWarpPosition;
     public CharacterController characterController;
